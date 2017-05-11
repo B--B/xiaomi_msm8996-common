a25c2bebbe3385eb800698aee798f817ef809f08
Balázs Triszka <balika011@gmail.com>
2017-05-11 03:19:29 +0200

    msm8996-common: qcom-caf: audio: Add open source HAL for Elliptic Ultrasound
    Author: Balázs Triszka <balika011@gmail.com>
    Date:   Thu May 11 03:19:29 2017 +0200
    
        msm8998: ultrasound: Initial open source hal for Elliptic Ultrasound
    
        * Needed for proximity sensor on Xiaomi Mi MIX
    
        Change-Id: Iaef2266bc1b853d7a9d1e2a906014c6c91019d5f
        Signed-off-by: Balázs Triszka <balika011@gmail.com>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat Feb 10 00:28:00 2018 +0200
    
        msm8998: ultrasound: Remove unused code
    
         * Params ultrasound_set_manual_calibration and ultrasound_set_sensitivity
           do not exist.
    
        Change-Id: I0004949db19b6ab7d49f20e422984e06a970cfe9
    
    Author: Demon Singur <demonsingur@gmail.com>
    Date:   Sat Apr 21 09:08:03 2018 +0000
    
        msm8998: hal: Update ultrasound route hacks
    
        Change-Id: If002503dfba0f005f73a4455d68bbcce9d2f617e
    
    Change-Id: I119316f264afbda9faf24950edfbca3891aa9769
    
    Author: Vol Zhdanov <wight554@gmail.com>
    Date:   Fri Feb 1 02:16:26 2019 +0000
    
        hal: fix channels swap for elliptic ultrasound devices
    
        Change-Id: Id45b075c4ad098c95fcb617da10c56d38565b9c8
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Tue Nov 19 20:48:55 2019 +0200
    
        ultrasound: Use log/log.h instead of cutils/log.h
    
        Change-Id: Ic35476c4447a5afdc5bc4996323c55dde0c92bb5
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Thu Dec 5 20:12:21 2019 +0200
    
        ultrasound: Update for new persist path
    
        Change-Id: I8dfd97d0320abfc349aa3da84db5a210ed8c031f
    
    Change-Id: Icb18f5b41483d33188be103e13b6c915b6e681b8
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- qcom-caf/audio/hal/audio_hw.h
+++ qcom-caf/audio/hal/audio_hw.h
@@ -280,6 +280,10 @@ enum {
 
     /*In Car Communication Usecase*/
     USECASE_ICC_CALL,
+
+    /* Elliptic Ultrasound */
+    USECASE_AUDIO_ULTRASOUND_RX,
+    USECASE_AUDIO_ULTRASOUND_TX,
     AUDIO_USECASE_MAX
 };
 
