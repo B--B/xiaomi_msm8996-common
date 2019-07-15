7c6fc8a40d7654474bf6293279fb6418f15d147f
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    msm8996-common: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>
    
    Conflicts:
    	BoardConfigCommon.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -59,6 +59,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
