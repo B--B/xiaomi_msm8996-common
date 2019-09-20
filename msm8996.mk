33e8a54b18133c470b14564ee2fd80c6756ec271
xNombre <kartapolska@gmail.com>
2019-09-20 23:35:24 +0200

    msm8996: reflect rootdir changes
    Change-Id: I3ea9b917c7a3bf7b31b6d453eac49c4ee0908818
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -150,10 +150,18 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.class_main.sh \
+    init.class_late.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.power.rc \
     init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.qti.ims.sh \
+    init.recovery.qcom.rc \
+    init.device.rc \
     ueventd.qcom.rc
 
 # Display
