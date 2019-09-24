c4975afa5a22045a0fa48ea2fe1823a390f7ff0c
xNombre <kartapolska@gmail.com>
2019-09-24 15:28:42 +0200

    Remove obsolete NFC props
    Change-Id: I5348cba5eb8db3c2655ab8daa335062609bc72ef
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -124,10 +124,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.bservice_enable=true
 
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.nfc.port=I2C
-
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
