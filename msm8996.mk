fc0645c5ee94545d6f3ee541159cfbe2f8e3692e
dianlujitao <dianlujitao@gmail.com>
2019-10-27 16:17:32 +0100

    msm8996-common: Add missing packages after blobs update
    Change-Id: Ibb2aab60cb27e053ab78ad8ea4b9317bdee1f4b2
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -370,9 +370,15 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common_system \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier
