0dda73e622ecf1fd2b5d160e061e4fc66aee5771
dianlujitao <dianlujitao@gmail.com>
2019-10-27 15:43:22 +0100

    msm8996-common: Add missing RIL packages
    Change-Id: I7d0d93a93d22215606600fc1db9ef38b4f9e0cf5
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -350,6 +350,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
