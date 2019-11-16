f28add6a8e1a09043a47ff631ac9fa866f8cde62
xNombre <kartapolska@gmail.com>
2019-11-16 14:16:15 +0100

    msm8996-common: init: Hax for 'internal problem in your device'
    Yeah yeah... I don't like this message.
    
    Change-Id: Id2e8a59f1b0a7840ca895e757b067c6f5c65723e
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- init/init_msm8996.cpp
+++ init/init_msm8996.cpp
@@ -28,7 +28,8 @@
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
 
 #include <android-base/properties.h>
@@ -41,6 +42,17 @@ using android::init::property_set;
 
 char const *heapminfree;
 char const *heapmaxfree;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 void check_device()
 {
@@ -69,4 +81,5 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", "0.75");
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("ro.build.fingerprint", android::base::GetProperty("ro.vendor.build.fingerprint", "").c_str());
 }
