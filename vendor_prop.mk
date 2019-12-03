46c96e78db74bac1d0f20b5833335775097e471a
xNombre <kartapolska@gmail.com>
2019-12-03 20:14:05 +0100

    msm8996-common: Add lmkd props
     * Kang lmkd props from newest google devices (aka Pixels).
    
     * This makes lmkd spread its wings and become a multitasking beast over kernel side LMK.
    
    Change-Id: Ia4e5c84d887eb2071cc969afd4c6455aa7e6b2e6
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -206,3 +206,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Set lmkd options
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
