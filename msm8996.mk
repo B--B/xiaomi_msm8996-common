825183a29f6597e6a959c43e0d871a3300ac11eb
dianlujitao <dianlujitao@lineageos.org>
2020-01-01 20:55:23 +0100

    Revert "op3: Create system copy of ims-ext-common and add to classpath"
    This reverts commit 57346a03a6b0035e9fcf235c97865022a41b9f40.
    
    Change-Id: Ic937b20297e06d79d392d4ab9a1ef42f498c03d2
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common_system \
+    ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -398,7 +398,6 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier
