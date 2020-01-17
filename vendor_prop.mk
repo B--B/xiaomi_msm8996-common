766cabbb19d8ecda6569d4d6bc2b9e394a2cef7d
xNombre <kartapolska@gmail.com>
2020-01-17 15:27:56 +0100

    msm8996-common: Remove depreciated vendor.gralloc.enable_fb_ubwc
    Change-Id: I9d8b07ddebefee3fa4b76afcbb7e428672dc2f80
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,7 +108,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.perf_hint_window=50 \
-    vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.recomputecrop=0
 
 # Surfaceflinger
