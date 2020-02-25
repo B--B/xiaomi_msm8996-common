7ee737acc49dfe1561b0a76440d58906cc8716f0
xNombre <kartapolska@gmail.com>
2020-02-25 22:46:24 +0100

    msm8996-common: Build missing camera library
    This fixes:
    "ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found"
    
    Change-Id: I67d6ffd7671a9a3131d8c2f3d194ac9526f8f6bd
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -146,6 +146,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
     camera.msm8996 \
     libmm-qcamera \
     libfui \
