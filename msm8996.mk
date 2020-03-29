d553e370451d822f654a54a45d5a5ce7a7660ace
xNombre <kartapolska@gmail.com>
2020-03-29 17:27:40 +0200

    msm8996-common: Prepare display config files for new blobs
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ib350344bece6cbbed4ddd8e283e682066ac866f4

--- msm8996.mk
+++ msm8996.mk
@@ -192,7 +192,8 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/calib.cfg:system/etc/calib.cfg
+    $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_config.cfg \
+    $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/sdr_config.cfg
 
 # DRM
 PRODUCT_PACKAGES += \
