d3ced0745e8b73a22e8681b1412d22adf319c766
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    msm8996-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>
    
    Conflicts:
    	msm8996.mk

--- msm8996.mk
+++ msm8996.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
+    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf \
     wifi-mac-generator
