1ad0b08b53b62e41700312f58929d61f22a55b46
mike dooley <mdooley@google.com>
2020-06-05 00:10:00 +0200

    msm8996-common: Enabling sound trigger V2_2 for msm8996 targets
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
