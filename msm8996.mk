78513c332d00ddd4d14590a9c78a60cfcd32fe40
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    msm8996-common: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>
    
    Conflicts:
    	msm8996.mk

--- msm8996.mk
+++ msm8996.mk
@@ -226,7 +226,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # Input
 PRODUCT_COPY_FILES += \
