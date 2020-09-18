ee94c534a5ea9927839ff044171246f5d8256d21
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    msm8996-common: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,7 +185,7 @@ VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
