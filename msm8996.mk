80296db7ec2e7426bde3b40d20c85e09c00627fe
xNombre <kartapolska@gmail.com>
2020-11-21 18:23:08 +0100

    msm8996-common: Update GPS hal to LA.UM.9.6.2.r1-04100-89xx.0
    Import GPS hal from tag LA.UM.9.6.2.r1-04100-89xx.0 and modify it to support msm8996.
    Import configs from LA.UM.9.6.2.r1-04100-89xx.0 BSP.
    Decrease logging level and add google supl for faster location fix.
    Bump hidl and hal versions and clean manifest entries.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -215,13 +215,15 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor \
+    libwifi-hal-ctrl \
     libbatching \
     libgeofencing \
     libgnss \
     libsensorndkbridge \
-    libwifi-hal-ctrl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
@@ -229,7 +231,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
+    $(LOCAL_PATH)/gps/etc/gnss_antenna_info.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss_antenna_info.conf
 
 # Healthd
 PRODUCT_PACKAGES += \
