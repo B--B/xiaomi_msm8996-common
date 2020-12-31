132db186141c8c9badde289f9e4887877985f724
Bruno Martins <bgcngm@gmail.com>
2020-12-31 00:46:08 +0000

    msm8996-common: Use CodeAurora ImsService implementation for RCS
    There is no evidence that Qualcomm devices running a rather recent IMS
    stack need to include AOSP RCS packages. RCS already worked before this
    change and continues to do so, but the main goal was to get rid of some
    of the RCS related log spam, which was accomplished here.
    
    Change-Id: I219546e29d288373877e9bcd194a60ef5e09f224
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -335,12 +335,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Recorder
 
-# RCS
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
