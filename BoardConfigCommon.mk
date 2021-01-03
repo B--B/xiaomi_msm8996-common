9db1ff8e9ae3f7d2dd582056c895e4a4d7390f28
xNombre <kartapolska@gmail.com>
2021-01-03 14:31:06 +0100

    msm8996: Enable service_locator
    It should be explicitly enabled in 4.4
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 kpti=off
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
