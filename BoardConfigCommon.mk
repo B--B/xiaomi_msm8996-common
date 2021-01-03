a6049e8e56001c46dc768b6761094a2b1f6e8eca
xNombre <kartapolska@gmail.com>
2021-01-03 14:32:52 +0100

    msm8996: Increase usb autosuspend delay
    According to this commit: https://github.com/LineageOS/android_device_google_crosshatch/commit/0134ae93aa6ce7961cd79fd083248d3e8aa0294f
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 kpti=off
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
