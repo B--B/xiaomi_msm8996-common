269d16dd86a9ab1fffedf2139cbb189eccb5d947
Wei Wang <wvw@google.com>
2021-06-14 22:03:10 -0700

    msm8996-common: Fix objects that are derived from refbase
    Fixes: ag/14313466
    Bug: 177493042
    Bug: 191163855
    Test: Build
    Change-Id: I94812997a8214b77a2e1d0bcf90ef62205c5adf6
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -83,7 +83,7 @@ Power::Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> d
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(DEBUG) << "Power setMode: " << toString(type) << " to: " << enabled;
     ATRACE_INT(toString(type).c_str(), enabled);
-    PowerSessionManager::getInstance().updateHintMode(toString(type), enabled);
+    PowerSessionManager::getInstance()->updateHintMode(toString(type), enabled);
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE:
             ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
