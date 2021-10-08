3754b4a7af68a057845a45a2f148fb0e143bf921
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 09:48:37 -0400

    msm8996-common: ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Beryllium was launched with Oreo (API level 26), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Change-Id: I7d7726f2c887fdc607c6bcede288e36681073fd9
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -134,6 +134,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
