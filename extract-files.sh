84a602949175ed9dbfe0bdaae8f86ca911798095
Marco Zanin <mrczn.bb@gmail.com>
2022-02-04 19:42:21 +0100

    Sync extract-files.sh and setup-makefiles.sh with los
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -22,26 +12,81 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+    system_ext/etc/init/dpmd.rc)
+        sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/com.qti.dpmframework.xml)
+        ;&
+    system_ext/etc/permissions/dpmapi.xml)
+        sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/qcrilhook.xml)
+        ;&
+    system_ext/etc/permissions/telephonyservice.xml)
+        sed -i "s/\/system\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/qti_libpermissions.xml)
+        sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
+        ;;
+    system_ext/lib64/libdpmframework.so)
+        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        ;;
+    vendor/bin/imsrcsd)
+        sed -i "s/libhidltransport.so/libbase_shim.so\x00\x00\x00\x00/" "${2}"
+        ;;
+    vendor/bin/slim_daemon)
+        "${PATCHELF}" --replace-needed "android.frameworks.sensorservice@1.0.so" "android.frameworks.sensorservice@1.0-v27.so" "${2}"
+        ;;
+    vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so)
+        sed -i "s/libhidltransport.so/libbase_shim.so\x00\x00\x00\x00/" "${2}"
+        ;;
+    vendor/lib64/hw/vulkan.msm8996.so)
+        sed -i "s/vulkan.msm8953.so/vulkan.msm8996.so/g" "${2}"
+        ;;
+    vendor/lib64/lib-uceservice.so)
+        sed -i "s/libhidltransport.so/libbase_shim.so\x00\x00\x00\x00/" "${2}"
+        ;;
+    vendor/lib64/libsettings.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v28.so" "${2}"
+        ;;
+    vendor/lib64/vendor.qti.gnss@1.0_vendor.so)
+        "${PATCHELF}" --replace-needed "android.hardware.gnss@1.0.so" "android.hardware.gnss@1.0-v27.so" "${2}"
+        ;;
+    vendor/lib/hw/vulkan.msm8996.so)
+        sed -i "s/vulkan.msm8953.so/vulkan.msm8996.so/g" "${2}"
+        ;;
+    vendor/lib/libwvhidl.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v28.so" "${2}"
+        ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
 ONLY_COMMON=
-SECTION=
+ONLY_TARGET=
 KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -o | --only-common )
-                ONLY_COMMON=false
+        --only-common )
+                ONLY_COMMON=true
+                ;;
+        --only-target )
+                ONLY_TARGET=true
                 ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
@@ -64,64 +109,19 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    # Rename msm8953 to msm8996
-    vendor/lib/hw/vulkan.msm8996.so | vendor/lib64/hw/vulkan.msm8996.so)
-        sed -i "s|vulkan.msm8953.so|vulkan.msm8996.so|g" "${2}"
-        ;;
-
-    vendor/lib/hw/activity_recognition.msm8996.so | vendor/lib64/hw/activity_recognition.msm8996.so)
-        sed -i "s|activity_recognition.msm8953.so|activity_recognition.msm8996.so|g" "${2}"
-        ;;
-
-    # Camera hax
-    vendor/lib/libmmcamera2_stats_modules.so)
-        sed -i "s|libgui.so|libfui.so|g" "${2}"
-        sed -i "s|/data/misc/camera|/data/vendor/qcam|g" "${2}"
-        patchelf --remove-needed libandroid.so "${2}"
-        ;;
-
-    vendor/lib/libmpbase.so)
-        patchelf --remove-needed libandroid.so "${2}"
-        ;;
-
-   vendor/lib/libmm-qcamera.so | vendor/lib/libmmcamera2_cpp_module.so | vendor/lib/libmmcamera2_iface_modules.so | vendor/lib/libmmcamera2_imglib_modules.so | vendor/lib/libmmcamera2_mct.so | vendor/lib/libmmcamera2_pproc_modules.so | vendor/lib/libmmcamera2_stats_algorithm.so | vendor/lib/libmmcamera_dbg.so | vendor/lib/libmmcamera_hvx_grid_sum.so | vendor/lib/libmmcamera_hvx_zzHDR.so | vendor/lib/libmmcamera_imglib.so | vendor/lib/libmmcamera_isp_mesh_rolloff44.so | vendor/lib/libmmcamera_pdaf.so | vendor/lib/libmmcamera_pdafcamif.so | vendor/lib/libmmcamera_tintless_algo.so | vendor/lib/libmmcamera_tintless_bg_pca_algo.so | vendor/lib/libmmcamera_tuning.so)
-        sed -i "s|/data/misc/camera|/data/vendor/qcam|g" "${2}"
-        ;;
-
-    vendor/lib/libmmcamera2_sensor_modules.so)
-        sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "${2}"
-        sed -i "s|/data/misc/camera|/data/vendor/qcam|g" "${2}"
-        ;;
-
-    vendor/bin/mm-qcamera-daemon)
-        sed -i "s|/data/vendor/camera/cam_socket%d|/data/vendor/qcam/camer_socket%d|g" "${2}"
-        ;;
-
-    # Assign all daemons to system-background cpuset
-    vendor/etc/init/*.rc)
-        vi -escwq "${2}"
-        echo "    writepid /dev/cpuset/system-background/tasks" >> "${2}"
-        ;;
-
-    esac
-}
-
-# Initialize the helper for common device
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
-
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
