0a12965f53463c93719d270de0e9b225c94ff6b7
Marco Zanin <mrczn.bb@gmail.com>
2022-02-18 18:10:25 +0100

    msm8996-common: cleanup props
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -165,12 +165,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
     sdm.debug.disable_skip_validate=1 \
-    debug.sdm.support_writeback=0 \
     vendor.display.disable_rotator_split=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
-    vendor.display.perf_hint_window=50 \
-    debug.gralloc.gfx_ubwc_disable=0 \
     vendor.gralloc.enable_fb_ubwc=1
 
 # Surfaceflinger
@@ -179,8 +176,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.enable_gl_backpressure=1 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
@@ -195,22 +190,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.vidc.enc.disable.pq=true \
     vendor.video.disable.ubwc=1 \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    vendor.vidc.disable.split.mode=1 \
     mmp.enable.3g2=true \
     media.aac_51_output_enabled=true \
     mm.enable.smoothstreaming=true \
-    persist.mm.enable.prefetch=true \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0 \
-    mm.sec.enable.smoothstreaming=true \
-    media.stagefright.audio.sink=280 \
-    vendor.mm.enable.qcom_parser=1048575 \
-    vendor.vidc.debug.level=0 \
-    vendor.vidc.disable.split.mode=1 \
-    media.settings.xml=/system/vendor/etc/media_profiles_vendor.xml
+    debug.stagefright.omx_default_rank=0
 
 # Nitz
 PRODUCT_PROPERTY_OVERRIDES += \
