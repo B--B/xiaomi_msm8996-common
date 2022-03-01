02667f4663c3d14875f2e1c092fb7c9bc6d4eac4
Marco Zanin <mrczn.bb@gmail.com>
2022-03-02 00:15:46 +0100

    ConfigPanel: updates and fixes for Android S
    * Use Theme.SubSettingsBase for theme
    * Replace PreferenceActivity with CollapsingToolbarBaseActivity
    * Remove onOptionsItemSelected
    * Update manifest
    * Compile with S SDK
    * Bump app version to 2.7
    * Updated and fixed vector for Android 12 Settings
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -13,7 +13,8 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference
+    androidx.preference_preference \
+    org.lineageos.settings.resources
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
