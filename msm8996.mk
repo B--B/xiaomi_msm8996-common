f1cdd5be71e5bbbf0c10f0209840dc50762b5b50
Marco Zanin <mrczn.bb@gmail.com>
2022-03-11 00:52:09 +0100

    msm8996-common: disable APEX compression
    There are multiple reports of kernel panics on devices with kernel 4.4/4.9 + ext4
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -23,8 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
 
-# Enable updatable APEXes
+# APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
