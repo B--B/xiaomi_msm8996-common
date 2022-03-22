772924661bb72de7d2da0aff59d682d783937082
Marco Zanin <mrczn.bb@gmail.com>
2022-03-23 00:36:39 +0100

    msm8996-common: don't build android.hardware.power.stats@1.0-service.mock
    Does not work on our device
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -357,8 +357,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-msm8996-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.xiaomi-msm8996-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
