21828f4610a72c5756cfe954a24e14d7abb1b3f3
Marco Zanin <mrczn.bb@gmail.com>
2022-03-24 21:24:22 +0100

    msm8996-common: set density with TARGET_SCREEN_DENSITY flag
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -172,7 +172,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Surfaceflinger
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
