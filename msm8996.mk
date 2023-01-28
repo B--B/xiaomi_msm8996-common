b2b46f193356d525ac39b3a4eb4527ffabb5ee50
Marco Zanin <mrczn.bb@gmail.com>
2023-01-28 20:57:38 +0100

    msm8996-common: pin SystemUIGoogle instead of AOSP version
    Also add SystemUIGoogle to PRODUCT_DEXPREOPT_SPEED_APPS
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
