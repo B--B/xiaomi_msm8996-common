80fedd5eee63e4a8ab485f78a4cd56d2f6bb8ad4
Marco Zanin <mrczn.bb@gmail.com>
2023-04-26 16:59:31 +0200

    msm8996-common: update DRM from Coral TPP1.220114.016/8158200
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
 # For config.fs
