2e34ba0fa17e45ec7e03909d7413eac5c5f55774
Marco Zanin <mrczn.bb@gmail.com>
2023-10-21 02:50:41 +0200

    msm8996-common: GPS: update from LA.UM.10.6.2.r1-02500-89xx.0
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -3,26 +3,25 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 # Set required flags
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-unused-parameter \
-    -Wno-macro-redefined \
-    -Wno-reorder \
-    -Wno-missing-braces \
-    -Wno-self-assign \
-    -Wno-enum-conversion \
-    -Wno-logical-op-parentheses \
-    -Wno-null-arithmetic \
-    -Wno-null-conversion \
-    -Wno-parentheses-equality \
-    -Wno-undefined-bool-conversion \
-    -Wno-tautological-compare \
-    -Wno-switch \
-    -Wno-date-time
+    -Wno-error=unused-parameter \
+    -Wno-error=macro-redefined \
+    -Wno-error=reorder \
+    -Wno-error=missing-braces \
+    -Wno-error=self-assign \
+    -Wno-error=enum-conversion \
+    -Wno-error=logical-op-parentheses \
+    -Wno-error=null-arithmetic \
+    -Wno-error=null-conversion \
+    -Wno-error=parentheses-equality \
+    -Wno-error=undefined-bool-conversion \
+    -Wno-error=tautological-compare \
+    -Wno-error=switch \
+    -Wno-error=date-time
 
 GNSS_HIDL_VERSION = 2.1
 
 GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += msm8937
 GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += msm8953
-GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += msm8996
 GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += msm8998
 GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += apq8098_latv
 GNSS_HIDL_LEGACY_MEASURMENTS_TARGET_LIST += sdm710
@@ -37,9 +36,6 @@ endif
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-GNSS_SANITIZE := cfi bounds null unreachable integer
-# Activate the following two lines for regression testing
-#GNSS_SANITIZE += address
-#GNSS_SANITIZE_DIAG := $(GNSS_SANITIZE)
+GNSS_SANITIZE_DIAG := cfi bounds null unreachable integer address
 
 endif # ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
