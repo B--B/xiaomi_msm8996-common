4c0fd7f0e3083ee68a381fa2419492ccec354bba
Marco Zanin <mrczn.bb@gmail.com>
2023-10-30 22:29:03 +0100

    msm8996-common: build vendor variants of libprotobuf-cpp-full-3.9.1 and libprotobuf-cpp-lite-3.9.1
    Needed by some RIL and DRM blobs
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -353,7 +353,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \
