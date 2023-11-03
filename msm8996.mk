7920490b3940646e51c924c491654e1b3d4c54f5
Marco Zanin <mrczn.bb@gmail.com>
2023-11-03 17:23:32 +0100

    msm8996-common: switch to legacy wifi HAL
    Old service has been deprecated and removed on Android U
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -449,7 +449,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     libwpa_client \
     hostapd \
     readfem \
