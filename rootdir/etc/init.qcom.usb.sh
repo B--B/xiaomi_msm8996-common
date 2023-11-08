dcfcec6c850fd7da2cecf41517fb688c3f5e17cf
Marco Zanin <mrczn.bb@gmail.com>
2023-11-08 23:51:19 +0100

    msm8996-common: USB scripts: merge updates from LA.UM.9.6.3.r1-06200-89xx.0
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -1,5 +1,5 @@
 #!/vendor/bin/sh
-# Copyright (c) 2012-2018, 2020 The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2018, 2020-2021 The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -68,9 +68,9 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a "$(getprop ro.build.type)" 
 		    ;;
 		    *)
 	            case "$target" in
-#	              "msm8996")
-#	                  setprop persist.vendor.usb.config diag,serial_cdev,serial_tty,rmnet_ipa,mass_storage,adb
-#		      ;;
+	              "msm8996")
+	                  setprop persist.vendor.usb.config diag,serial_cdev,serial_tty,rmnet_ipa,mass_storage,adb
+		      ;;
 	              "msm8909")
 		          setprop persist.vendor.usb.config diag,serial_smd,rmnet_qti_bam,adb
 		      ;;
@@ -88,7 +88,7 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a "$(getprop ro.build.type)" 
 			               esac
 			    fi
 		      ;;
-	              "msm8953" | "msm8996")
+	              "msm8953")
 			      if [ -d /config/usb_gadget ]; then
 				      setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,adb
 			      else
@@ -103,6 +103,9 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a "$(getprop ro.build.type)" 
 		      ;;
 	              "msmnile" | "sm6150" | "trinket" | "lito" | "atoll" | "bengal" | "lahaina" | "holi")
 			  setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,qdss,adb
+		      ;;
+	              "monaco")
+		          setprop persist.vendor.usb.config diag,qdss,rmnet,adb
 		      ;;
 	              *)
 		          setprop persist.vendor.usb.config diag,adb
@@ -139,8 +142,15 @@ esac
 
 # check configfs is mounted or not
 if [ -d /config/usb_gadget ]; then
-	model=`getprop ro.product.model`
-	setprop vendor.usb.product_string "$model"
+	# Chip-serial is used for unique MSM identification in Product string
+	msm_serial=`cat /sys/devices/soc0/serial_number`;
+	# If MSM serial number is not available, then keep it blank instead of 0x00000000
+	if [ "$msm_serial" != "" ]; then
+		msm_serial_hex=`printf %08X $msm_serial`
+	fi
+
+	machine_type=`cat /sys/devices/soc0/machine`
+	setprop vendor.usb.product_string "$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
 
 	# ADB requires valid iSerialNumber; if ro.serialno is missing, use dummy
 	serialnumber=`cat /config/usb_gadget/g1/strings/0x409/serialnumber 2> /dev/null`
